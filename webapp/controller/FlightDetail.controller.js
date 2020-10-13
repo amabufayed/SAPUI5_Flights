0e84236c3fdead30f5cdcb9a1312f9bc0d43bcb2
Matjaz <besednjak.matjaz@gmail.com>
2020-10-13 15:35:45 +0200

    added new data model. need to fix calculation of seat spots

--- webapp/controller/FlightDetail.controller.js
+++ webapp/controller/FlightDetail.controller.js
@@ -12,13 +12,57 @@ sap.ui.define([
 
         onInit: function () {
             this._oRouter = this.getRouter();
-			this._oRouter.getRoute("flightDetail").attachPatternMatched(this._routePatternMatched, this);
+            this._oRouter.getRoute("flightDetail").attachPatternMatched(this._routePatternMatched, this);
+
+            var freeSeatsModel = new JSONModel({
+                "econSeatsFree": 1,
+                "businessSeatsFree": 2,
+                "firstSeeatsFree": 3
+            });
+
+            this.setModel(freeSeatsModel, "freeSeatsModel");
         },
         //get connid from url
         _routePatternMatched: function (oEvent) {
+
+            //get data for selected SFLIGHTS table
             this.connid = oEvent.getParameter("arguments").connid;
-            this.getView().bindElement("/FlightAvaliabilitySet('" + this.connid + "')");
-            
+            //this.getView().bindElement("/FlightAvaliabilitySet('" + this.connid + "')");
+
+            this.oModel = this.getModel();
+
+            this.oModel.read("/FlightAvaliabilitySet('" + this.connid + "')", {
+				success: function (oRetrievedResult) {
+					var model = new JSONModel(oRetrievedResult);
+					this.getView().setModel(model, "flightInfo");
+				}.bind(this)
+            })
+
+            //get data for selected SPFLI table
+            this.oModel.read("/FlightsSet('" + this.connid + "')", {
+				success: function (oRetrievedResult) {
+					var model = new JSONModel(oRetrievedResult);
+					this.getView().setModel(model, "flights");
+				}.bind(this)
+            });
+
+            setTimeout(() => {  this.getSeatsSpots(); }, 200);
+            //this.getSeatsSpots();
+        },
+
+        getSeatsSpots: function() {
+            var freeSeatsModel = this.getModel("freeSeatsModel");
+            var oDataModel = this.getModel("flightInfo");
+            //freeSeatsModel.setProperty("/econSeatsFree", this.getModel("flightInfo").getProperty("/Seatsmax") - this.getModel("flightInfo").getProperty("/Seatsocc"))
+
+            //console.log(oDataModel.getProperty("/Connid"));
+            console.log(oDataModel.getProperty("/Seatsmax") - oDataModel.getProperty("/Seatsocc"));
+            freeSeatsModel.setProperty("/econSeatsFree", (oDataModel.getProperty("/Seatsmax") - oDataModel.getProperty("/Seatsocc")))
+            console.log(freeSeatsModel.getProperty("/econSeatsFree"));
+        },
+
+        bookFlight: function() {
+            console.log("booked flight")
         }
 	});
 });
